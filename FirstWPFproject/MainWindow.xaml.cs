68822bc85ae09a4694b11bc22e517121ce5b076e
Said Shah <sshah@andculture.com>
2019-08-27 14:20:37 -0400

    initial commit

---
+++ FirstWPFproject/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace FirstWPFproject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // to change the title for the project
+            Title = "hello world";
+            // to change the start up of the screen
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
